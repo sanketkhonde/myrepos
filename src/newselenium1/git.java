c6f1ea6531fdfc7b2f03c50123565ea6771bbef2
sanket <sanket@SNAKETG3>
2022-08-01 08:50:01 +0530

    new repo

--- src/newselenium1/git.java
+++ src/newselenium1/git.java
@@ -1,7 +1,26 @@
 package newselenium1;
 
 public class git {
-public static void main(String[] args) {
-	System.out.println("hello world!");
-}
+	
+	
+	
+	class a{
+		public void mainw() {
+			System.out.println("jhelo");
+			
+		}
+	}
+	public static void main(String[] args) {
+
+		StringBuffer SA = new StringBuffer("Test");
+		StringBuffer SB = SA; //test
+		SA = SA.append("Leaf"); //testleaf
+		System.out.println(SB);//
+		System.out.println(SA); //testleaf
+//a n=new a();
+git n=new git();
+git.a g=n.new a();
+g.mainw();
+		// System.out.println(a);
+	}
 }
